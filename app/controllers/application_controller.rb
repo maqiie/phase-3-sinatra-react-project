c2297016ae73b444ec30b344a91418ace71b96fc
maqiie <candiemp2580@gmail.com>
2023-03-06 10:26:47 +0300

    worked on the server

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,10 @@ class ApplicationController < Sinatra::Base
   get "/" do
     { message: "Good luck with your project!" }.to_json
   end
-get '/users'
+get '/users' do
+  @users=User.all.to_json
+end
+get '/projects' do 
+  @projects=Project.all.to_json
+end
 end
